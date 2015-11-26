b522863925d7ff22d753fb8a90e7ea3cf0e108ea
Vitaliy Vlasov <siphiuel@gmail.com>
2015-11-26 16:55:27 +0200

    v.py

---
+++ scripts/v.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from neovim import attach
+import os, sys
+
+args = sys.argv[1:]
+#if not args:
+ #   print "Usage: {} <filename> ...".format(sys.argv[0])
+ #   sys.exit(1)
+
+addr = os.environ.get("NVIM_LISTEN_ADDRESS")
+if not addr:
+    os.execvp('nvim', ('nvim', ) + tuple(args))
+
+nvim = attach("socket", path=addr)
+def normalizePath(name):
+    return os.path.abspath(name).replace(" ", "\\ ")
+
+def openFiles():
+    # To escape terminal mode. Not sure if bug.
+    nvim.feedkeys('',"n")
+    for x in args:
+        nvim.command("drop {}".format(normalizePath(x)))
+
+openFiles()
